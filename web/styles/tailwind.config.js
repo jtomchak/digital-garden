0fcb3044414f48653a4cd59b0069f679ee5aba30
Jesse Tomchak <jtomchak@gmail.com>
2021-10-05 15:42:01 -0700

    update tailwind

--- web/styles/tailwind.config.js
+++ web/styles/tailwind.config.js
@@ -1,3 +1,7 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+const colors = require("tailwindcss/colors");
+const typographyOverride = require("./typography");
+
 module.exports = {
   mode: "jit",
   purge: {
@@ -8,11 +12,33 @@ module.exports = {
   },
   theme: {
     extend: {
+      typography: typographyOverride,
       colors: {
         change: "black",
       },
     },
+    colors: {
+      gray: colors.coolGray,
+      blue: colors.lightBlue,
+      red: colors.rose,
+      pink: colors.fuchsia,
+    },
+    extend: {
+      fontFamily: {
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+      },
+      fontWeight: ["hover", "focus"],
+    },
+    variants: {
+      wordBreak: ["responsive", "group-hover"],
+      textOverflow: ["responsive", "group-hover"],
+    },
   },
   variants: {},
-  plugins: [],
+  plugins: [
+    require("@tailwindcss/typography")({
+      modifiers: [],
+    }),
+    require("@tailwindcss/ui"),
+  ],
 };
