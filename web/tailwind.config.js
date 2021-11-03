5c5b0e46614bfe0eec0d05fd5d2e91d8c4e3ae45
Jesse Tomchak <jtomchak@gmail.com>
2021-11-03 12:52:33 -0700

    tailwind config

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,19 +1,23 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
-const typographyOveride = require("./styles/typography");
+const colors = require("tailwindcss/colors");
+const typographyOverride = require("./styles/typography");
+
 module.exports = {
-  darkMode: "media",
-  // mode: "jit",
-  purge: {
-    purge: ["./src/**/*.{js,ts,jsx,tsx}", "./pages/**/*.{js,ts,jsx,tsx}"],
-  },
+  mode: "jit",
+  purge: [
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {
-      colors: {
-        dark: "#24283b",
-      },
-
-      typography: typographyOveride,
+      typography: typographyOverride,
     },
+  },
+  colors: {
+    gray: colors.coolGray,
+    blue: colors.lightBlue,
+    red: colors.rose,
+    pink: colors.fuchsia,
   },
   extend: {
     fontFamily: {
@@ -25,8 +29,10 @@ module.exports = {
     wordBreak: ["responsive", "group-hover"],
     textOverflow: ["responsive", "group-hover"],
   },
-  variants: {
-    typography: ["dark"],
-  },
-  plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
+  plugins: [
+    require("@tailwindcss/typography")({
+      modifiers: [],
+    }),
+    require("@tailwindcss/ui"),
+  ],
 };
