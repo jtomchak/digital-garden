a8700d13407b013c734ce4464b1dfd3e1928fb10
Jesse Tomchak <jtomchak@gmail.com>
2022-12-01 15:02:35 -0700

    key value

--- web/pages/index.jsx
+++ web/pages/index.jsx
@@ -14,7 +14,7 @@ const Home = ({ articles }) => {
     <div className="text-gray-200 flex flex-col  justify-center items-center">
       <div className="relative">
         {articles.map((a) => (
-          <Article key={a.id} article={a} />
+          <Article key={a._id} article={a} />
         ))}
       </div>
     </div>
